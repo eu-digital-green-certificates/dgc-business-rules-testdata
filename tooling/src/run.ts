ebd9e1f9bb7ccdd121c75760978a9febb4eda1a7
Meinte Boersma <meinte.boersma@gmail.com>
2021-06-21 20:15:53 +0200

    run rules' tests against CertLogic (JS), using Mocha

--- tooling/src/run.ts
+++ tooling/src/run.ts
@@ -1,5 +1,5 @@
 import { evaluate } from "certlogic-js"
-const deepEqual = require("deep-equal")
+const { deepEqual, fail } = require("chai").assert
 
 import { gatherRuleSetsAsMap, readRuleJson, readRuleTestJson } from "./repo-struct"
 import { validateRule } from "./validate"
@@ -13,36 +13,35 @@ const ruleSetsMap = gatherRuleSetsAsMap()
 for (const ruleSetId in ruleSetsMap) {
     for (const ruleId in ruleSetsMap[ruleSetId]) {
         const ruleText = `rule "${ruleId}" in set "${ruleSetId}"`
-        const rule = readRuleJson(ruleSetId, ruleId)
-        const ruleValidation = validateRule(rule)
-        if (ruleValidation.schemaValidationsErrors.length > 0) {
-            console.error(`${ruleText} has schema validation errors:`)
-            console.error(asPrettyText(ruleValidation.schemaValidationsErrors))
-            console.error()
-        }
-        if (ruleValidation.logicValidationErrors.length > 0) {
-            console.error(`CertLogic expression in ${ruleText} has validation errors:`)
-            console.error(asPrettyText(ruleValidation.logicValidationErrors))
-            console.error()
-        }
-        if (ruleValidation.affectedFields) {
-            console.error(`${ruleText} specifies other affected fields than computed from its CertLogic expression (actual vs. computed):`)
-            console.error(asPrettyText(ruleValidation.affectedFields.actual))
-            console.error(asPrettyText(ruleValidation.affectedFields.computed))
-            console.error()
-        }
-        for (const testFile of ruleSetsMap[ruleSetId][ruleId].testFiles) {
-            const { name, payload, external, expected } = readRuleTestJson(ruleSetId, ruleId, testFile)
-            const actual = evaluate(rule.Logic, { payload, external })
-            if (!deepEqual(actual, expected)) {
-                console.error(`\t\ttest in file "${testFile}" of ${ruleText} doesn't evaluate to expected value, but to:`)
-                console.error(asPrettyText(actual))
-                console.error()
+        describe(ruleText, () => {
+            const rule = readRuleJson(ruleSetId, ruleId)
+            it("validates against rule's JSON Schema, other checks, and CertLogic spec", () => {
+                const ruleValidation = validateRule(rule)
+                if (ruleValidation.schemaValidationsErrors.length > 0) {
+                    console.error(`${ruleText} has schema validation errors:`)
+                    console.error(asPrettyText(ruleValidation.schemaValidationsErrors))
+                    fail()
+                }
+                if (ruleValidation.affectedFields) {
+                    console.error(`${ruleText} specifies other affected fields than computed from its CertLogic expression (actual vs. computed):`)
+                    console.error(asPrettyText(ruleValidation.affectedFields.actual))
+                    console.error(asPrettyText(ruleValidation.affectedFields.computed))
+                    fail()
+                }
+                if (ruleValidation.logicValidationErrors.length > 0) {
+                    console.error(`CertLogic expression in ${ruleText} has validation errors:`)
+                    console.error(asPrettyText(ruleValidation.logicValidationErrors))
+                    fail()
+                }
+            })
+            for (const testFile of ruleSetsMap[ruleSetId][ruleId].testFiles) {
+                const { name, payload, external, expected } = readRuleTestJson(ruleSetId, ruleId, testFile)
+                const actual = evaluate(rule.Logic, { payload, external })
+                it(`${(name || "<no name>")} (test-file=${testFile})`, () => {
+                    deepEqual(actual, expected, `test in file "${testFile}" of ${ruleText} doesn't evaluate to expected value`)
+                })
             }
-        }
-        console.log(`\tvalidated ${ruleText}`)
+        })
     }
-    const nRules = Object.keys(ruleSetsMap[ruleSetId]).length
-    console.log(`validated rules in rule set with id "${ruleSetId}" (${nRules} rule${nRules === 1 ? "" : "s"})`)
 }
 
