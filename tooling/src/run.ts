f0a8f1aff3b73a043f92081cdd3ffdc8a7eede12
Meinte Boersma <meinte.boersma@gmail.com>
2021-06-23 19:51:28 +0200

    catch errors during evaluation of CertLogic expressions to give proper feedback

--- tooling/src/run.ts
+++ tooling/src/run.ts
@@ -1,8 +1,8 @@
-import { evaluate } from "certlogic-js"
-const { deepEqual, fail } = require("chai").assert
+import {evaluate} from "certlogic-js"
+import {gatherRuleSetsAsMap, readRuleJson, readRuleTestJson} from "./repo-struct"
+import {validateRule} from "./validate"
 
-import { gatherRuleSetsAsMap, readRuleJson, readRuleTestJson } from "./repo-struct"
-import { validateRule } from "./validate"
+const { deepEqual, fail } = require("chai").assert
 
 
 const asPrettyText = (json: any) => JSON.stringify(json, null, 2)
@@ -36,9 +36,13 @@ for (const ruleSetId in ruleSetsMap) {
             })
             for (const testFile of ruleSetsMap[ruleSetId][ruleId].testFiles) {
                 const { name, payload, external, expected } = readRuleTestJson(ruleSetId, ruleId, testFile)
-                const actual = evaluate(rule.Logic, { payload, external })
                 it(`${(name || "<no name>")} (test-file=${testFile})`, () => {
-                    deepEqual(actual, expected, `test in file "${testFile}" of ${ruleText} doesn't evaluate to expected value`)
+                    try {
+                        const actual = evaluate(rule.Logic, {payload, external})
+                        deepEqual(actual, expected, `test in file "${testFile}" of ${ruleText} doesn't evaluate to expected value`)
+                    } catch (e) {
+                        fail(`exception occurred during evaluation of CertLogic expression: ${e}`)
+                    }
                 })
             }
         })
