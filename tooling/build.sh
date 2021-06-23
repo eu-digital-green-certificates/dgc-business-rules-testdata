bd94edfe3e0f782e5677990bd1ea97850d898615
Meinte Boersma <meinte.boersma@gmail.com>
2021-06-24 00:53:09 +0200

    (fix CI build problem)

--- tooling/build.sh
+++ tooling/build.sh
@@ -15,6 +15,7 @@ cd ../..
 curl https://raw.githubusercontent.com/eu-digital-green-certificates/dgc-gateway/main/src/main/resources/validation-rule.schema.json > validation-rule.schema.json
 
 # build and run validation and testing tooling:
+npm run clean
 npm install
 npm run build
 npm test
