20e31153a91ddf320889f19fbec2e66195b7cf97
Meinte Boersma <meinte.boersma@gmail.com>
2021-06-25 13:36:40 +0200

    implement compression of value sets using their new repo

---
+++ valuesets/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VALUESETS_REPO_NAME=ehn-dcc-valuesets
+
+git clone --depth 1 https://github.com/ehn-dcc-development/$VALUESETS_REPO_NAME.git
+# locally, use:   ln -s ../../$VALUESETS_REPO_NAME
+
+if [ -d "$VALUESETS_REPO_NAME" ]
+then
+  jq --slurp 'map( { (.valueSetId): .valueSetValues|keys }) | add' $VALUESETS_REPO_NAME/*.json > compressed-valueSets.json
+  echo "Compressed value sets."
+else
+  echo "expected $VALUESETS_REPO_NAME to exist"
+fi
+
