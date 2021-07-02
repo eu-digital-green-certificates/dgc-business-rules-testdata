c427866453dc87fe966a5a83095df95c38b20bc0
Meinte Boersma <meinte.boersma@gmail.com>
2021-07-02 14:57:55 +0200

    (add some verbosity to build scripts)

--- tooling/build.sh
+++ tooling/build.sh
@@ -5,14 +5,16 @@ REPO_NAME=dgc-testdata
 git clone --depth 1 https://github.com/eu-digital-green-certificates/$REPO_NAME.git
 # locally, use:   ln -s ../../$REPO_NAME .
 
-# get JSON Schema:
+echo "Downloading JSON Schema for rules..."
 curl https://raw.githubusercontent.com/eu-digital-green-certificates/dgc-gateway/main/src/main/resources/validation-rule.schema.json > schemas/validation-rule.schema.json
 
 mkdir -p ../out
 
-# build and run validation and testing tooling:
+echo "Building tooling..."
 npm run clean
 npm install
 npm run build
+
+echo "Running all tests (as Mocha unit tests)..."
 npm test
 
