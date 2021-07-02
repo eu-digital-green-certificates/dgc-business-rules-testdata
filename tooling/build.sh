27c88625376bc1d46835cb9b68b84c5579e93be4
Meinte Boersma <meinte.boersma@gmail.com>
2021-07-02 14:50:30 +0200

    fix CI things

--- tooling/build.sh
+++ tooling/build.sh
@@ -8,11 +8,11 @@ git clone --depth 1 https://github.com/eu-digital-green-certificates/$REPO_NAME.
 # get JSON Schema:
 curl https://raw.githubusercontent.com/eu-digital-green-certificates/dgc-gateway/main/src/main/resources/validation-rule.schema.json > schemas/validation-rule.schema.json
 
+mkdir -p ../out
+
 # build and run validation and testing tooling:
 npm run clean
 npm install
 npm run build
 npm test
-
-mkdir -p ../out
 
