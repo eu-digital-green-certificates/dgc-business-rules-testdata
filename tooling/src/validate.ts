a2807aea3790433b59e57332f89004e929d15b52
Meinte Boersma <meinte.boersma@gmail.com>
2021-07-27 16:10:50 +0200

    take validity range into account for rules' tests

--- tooling/src/validate.ts
+++ tooling/src/validate.ts
@@ -1,4 +1,5 @@
 import { version } from "certlogic-js"
+import { dateFromString } from "certlogic-js/dist/internals"
 import { dataAccesses, validateFormat } from "certlogic-js/dist/validation"
 import { gt } from "semver"
 
@@ -42,14 +43,22 @@ const validateMetaData = (rule: Rule) => {
     if (gt("0.7.5", rule.EngineVersion)) {
         errors.push(`EngineVersion ${rule.EngineVersion} must be 0.7.5 or newer`)
     }
-    if (rule.ValidTo) {
-        const validFrom = new Date(rule.ValidFrom)
-        const validTo = new Date(rule.ValidTo)
-        if (validFrom > validTo) {
-            errors.push(`ValidFrom ${rule.ValidFrom} must be before ValidTo ${rule.ValidTo}`)
+    if (!rule.ValidFrom) {
+        errors.push(`ValidFrom must be defined`)
+    } else {
+        const validFrom = dateFromString(rule.ValidFrom)
+        let nowPlus48hours = new Date()
+        nowPlus48hours.setUTCHours(nowPlus48hours.getUTCHours() + 48)
+        if (nowPlus48hours >= validFrom) {
+            console.warn(`\t[WARNING] a rule's ValidFrom has to be at least 48 hours in the future (at the moment of upload)`)
+        }
+        if (rule.ValidTo) {
+            const validTo = dateFromString(rule.ValidTo)
+            if (validFrom > validTo) {
+                errors.push(`ValidFrom ${rule.ValidFrom} must be before ValidTo ${rule.ValidTo}`)
+            }
         }
     }
-
     return errors
 }
 
