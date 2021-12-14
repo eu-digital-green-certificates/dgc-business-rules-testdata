d01b7215307505ee02dfacaa15759c22392d2f08
Meinte Boersma <meinte.boersma@gmail.com>
2021-12-14 22:34:35 +0100

    fix value sets build script for Bourne shell

--- valuesets/build.sh
+++ valuesets/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-valueSets=(country-2-codes.json \
+valueSets="country-2-codes.json \
   disease-agent-targeted.json\
   test-manf.json\
   test-result.json\
@@ -8,15 +8,15 @@ valueSets=(country-2-codes.json \
   vaccine-mah-manf.json\
   vaccine-medicinal-product.json\
   vaccine-prophylaxis.json\
-)
+"
 
-for vs in ${valueSets[@]}; do
+for vs in $valueSets; do
   curl "https://raw.githubusercontent.com/ehn-dcc-development/ehn-dcc-valuesets/main/$vs" > $vs
 done
 
-jq --slurp 'map( { (.valueSetId): .valueSetValues|keys }) | add' ${valueSets[@]} > valueSets.json
+jq --slurp 'map( { (.valueSetId): .valueSetValues|keys }) | add' $valueSets > valueSets.json
 
-for vs in ${valueSets[@]}; do
+for vs in $valueSets; do
   rm $vs
 done
 
